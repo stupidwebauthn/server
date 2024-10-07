eb078e42011d31f7ac3758b7f048fc389428337b
Lucian I. Last <li@last.nl>
2024-10-07 08:43:46 +0200

    Fix test

--- src/index.test.ts
+++ src/index.test.ts
@@ -5,7 +5,7 @@ import { expect, test } from "bun:test";
 const client = testClient(app);
 
 test("should be able to return root page", async () => {
-  const res = await client.index.$get();
+  const res = await client.auth.$get();
   expect(res.status).toEqual(200);
   expect(await res.text()).toEqual("Hello World!");
 });
